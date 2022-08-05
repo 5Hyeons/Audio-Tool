6adf23db5c905811f6947ea660a7be5c3b2abb7c
5Hyeons <ohs2251@naver.com>
2022-08-05 16:08:14 +0900

    chore

--- test.py
+++ test.py
@@ -4,9 +4,9 @@ import time
 g2p = G2p()
 
 start = time.time()
-t = '지칠줄 모르게 달려가는 그. 따라오는 3422 4명의 사람들. rlaew가 있다. 앓고 있다.'*555
+t = '지칠줄 모르게 달려가는 그. 따라오는 3422 4명의 사람들. rlaew가 있다. 앓고 있붙|안녕하세요.'
 # lst = [g2p(_t) for _t in t.split()]
-print(g2p(t, use_dict=False)[:120])
+print(g2p(t, use_dict=False)[:220])
 end = time.time()
 print(f'{round(end-start, 3)} passed during g2p')
 
